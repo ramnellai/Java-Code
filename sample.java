5b69483be49bc52afcd37180cf14281a5ff106e1
ramnellai <ramnath25@gmail.com>
2022-07-15 11:45:53 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,6 @@
 class sample{
      public static void main(String[] args) {
         System.out.println("Hi welcome!!!");
-    }
-       
+     }}
 
-}
+# TRY TO add next code i received thank you.
